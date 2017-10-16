1483bf081514984abc4df2139c2133fc244ec40e
Sam.Shen <samfec@163.com>
2017-10-16 11:54:20 +0800

    ,

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 /**
  * Created by SamMFFL on 2017/9/23.
  */
-
-import "babel-polyfill";
-flexible
+flexible // 页面布局弹性化
+import "babel-polyfill"; // 支持es6的API方法
 import './assets/styles/index.scss';
-
-import _ from 'lodash';
+// import _ from 'lodash';
 let $root = $('#root');
 
 console.log($root);
 
-console.log(123)
+console.log(123);
 try{alert([1,2,3].includes(1));}catch(e){
     alert(e);
 }
