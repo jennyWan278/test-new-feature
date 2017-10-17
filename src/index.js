b9e2aa1ae87a77835211c3bbbc5cefd956181650
Sam.Shen <samfec@163.com>
2017-10-17 13:59:01 +0800

    .

--- src/index.js
+++ src/index.js
@@ -7,18 +7,27 @@ import 'flexible';
 import './assets/styles/index.scss';
 // import './assets/styles/test.css';
 import _ from 'lodash';
-import react from 'react';
+import {Map, List} from 'immutable';
 
-console.log(_);
-console.log(react);
-let $root = $('#root');
+import addressList from './assets/data/addresslist.json';
 
-console.log($root);
+console.log(addressList);
+// let $root = $('#root');
+//
+// console.log($root);
+//
+// console.log(123)
+// // try{alert([1,2,3].includes(1));}catch(e){
+// //     alert(e);
+// // }
 
-console.log(123)
-try{alert([1,2,3].includes(1));}catch(e){
-    alert(e);
-}
 
+let obj = addressList.data.attributes;
+console.log('test', obj);
 
-import './another-module'
+let c =  _.cloneDeep(obj);
+// let c = deepCopy(obj);
+c['default'].isDefault = 1;
+c['others'].push(c.default);
+
+console.log('immutable', c);
